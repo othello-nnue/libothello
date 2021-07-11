3ac10ba1b32f681ef5320fa3375ba1d3a8eda8a5
leesongun@naver.com <leesongun@naver.com>
2021-07-11 21:52:30 +0900

    add small helper function

--- game/intrinsic.zig
+++ game/intrinsic.zig
@@ -10,3 +10,7 @@ pub inline fn pext(a: u64, b: u64) u64 {
 pub inline fn pdep(a: u64, b: u64) u64 {
     return @"llvm.x86.bmi.pdep.64"(a, b);
 }
+
+pub inline fn mul(x: u8, y: u8) u64 {
+    return pdep(x, 0x0101_0101_0101_0101) * y;
+}
