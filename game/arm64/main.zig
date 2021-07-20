94b685f3b9e479b340536fe697c39c6fc106870e
leesongun@naver.com <leesongun@naver.com>
2021-07-20 20:42:40 +0900

    fix arm

--- game/arm64/main.zig
+++ game/arm64/main.zig
@@ -3,13 +3,12 @@ const MASK = @import("./mask.zig").MASK;
 fn flip_positive(board: [2]u64, place: u6) u64 {
     var ret: u64 = 0;
     const t = @as(u64, 1) << place;
-    for (MASK2[place]) |mask| {
+    for (MASK[place]) |mask| {
         const o = (board[0] & mask[0]) >> 1;
         const u = (o ^ (o -% t)) & mask[0];
         const v = u & board[1] & mask[1];
         if (u == v) ret |= v;
     }
-
     return ret;
 }
 
