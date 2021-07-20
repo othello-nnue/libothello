83db93afb14babe7c688482425d248c7f252b9f5
leesongun@naver.com <leesongun@naver.com>
2021-07-20 19:03:21 +0900

    update script

--- install_zig.sh
+++ install_zig.sh
@@ -3,5 +3,6 @@ cd /tmp
 URL=$(wget -O - https://ziglang.org/download/index.json | jq -r '.master."'$(arch)'-linux".tarball' 2>&1)
 wget $URL
 tar xf $(basename $url)
-mv $(basename -s .tar.xz $url)/ "$HOME"/zig
+mkdir "$ZIG_DIR"
+mv $(basename -s .tar.xz $url)/ "$ZIG_DIR"
 cd -
