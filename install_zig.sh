060c29df492109a4e53e11295989ebc747ab523f
leesongun@naver.com <leesongun@naver.com>
2021-07-20 18:56:19 +0900

    fix bash script

--- install_zig.sh
+++ install_zig.sh
@@ -3,5 +3,5 @@ cd /tmp
 URL=$(wget -O - https://ziglang.org/download/index.json | jq -r '.master."'$(arch)'-linux".tarball' 2>&1)
 wget $URL
 tar xf $(basename $url)
-mv (basename -s .tar.xz $url)/ "$HOME"/zig
+mv $(basename -s .tar.xz $url)/ "$HOME"/zig
 cd -
