48a4b4a1ed6c556fca433b1d591c642a2cacc9ef
leesongun@naver.com <leesongun@naver.com>
2021-07-20 19:17:47 +0900

    cleanup ci

--- install_zig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /tmp
-URL=$(wget -O - https://ziglang.org/download/index.json | jq -r '.master."'$(arch)'-linux".tarball' 2>&1)
-wget $URL
-tar xf $(basename $url)
-mkdir "$ZIG_DIR"
-mv $(basename -s .tar.xz $url)/ "$ZIG_DIR"
-cd -
