7022c0209be378752b9c10cbf016215c6ab5583c
leesongun@naver.com <leesongun@naver.com>
2021-07-27 04:14:47 +0900

    it works

--- training/dag.jl
+++ training/dag.jl
@@ -1,3 +1,4 @@
+#need to denote pass moves some way
 State = Tuple{UInt64, UInt64}
 
 struct Policy
@@ -16,22 +17,18 @@ end
 
 TT = Dict{State, Node}
 
-
-function alpha(dict :: TT, node :: State)
-
-end
-
 #maybe change to AbstractFloat?
 #actually only lambda * policy matters
-function alpha(policy :: Vector{Float32}, value :: Vector{Float32}, lambda) Float32
+function pubar(policy :: Vector{Float32}, value :: Vector{Float32}, lambda) Float32
     #perform binary search...
     mina = max((value + lambda * policy)...)
     maxa = max(value...) + lambda
     while true
         a = (maxa + mina) / 2
-        val =  lambda * sum(policy ./ (a - value))
+        v =  policy ./ (a .- value)
+        val = lambda * sum(v)
         if val == 1 
-            return a
+            return lambda * v
         elseif val > 1
             mina = a
         elseif val < 1
@@ -43,8 +40,8 @@ end
 function pibar(dict :: TT, node :: State)
     node = dict[node]
     policy = (x -> x.policy).(node.child)
-    value = ((x -> x.value / x.visits)∘(x -> dict[x.state])).(node.child)
-    alpha_ = alpha(policy, value, node.visits ^ -0.5)
+    value = ((x -> -x.value / x.visits)∘(x -> dict[x.state])).(node.child)
+    return pibar (policy, value, node.visits ^ -0.5)
 end
 
 function expand(dict :: TT, node :: State, model)
