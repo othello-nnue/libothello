89524ffd6ed8f019f9f044f1ea814e7b488a2833
leesongun@naver.com <leesongun@naver.com>
2021-08-03 22:48:33 +0900

    add some tests

--- game/augmentation.zig
+++ game/augmentation.zig
@@ -1,48 +1,54 @@
+const mul = @import("./utils.zig").mul;
 fn deltaswap(x: u64, shift: u6, mask: u64) u64 {
     const y = (x ^ (x >> shift)) & mask;
     return x ^ y ^ @shlExact(y, shift);
 }
 
-fn deltaswap3(x: u64, shift: u6, mask: u64) u64 {
-    return (x & ~(mask | mask << shift)) | ((x >> shift) & mask) | ((x & mask) << shift);
-}
-
-fn deltaswap(x: u64, shift: u6, mask: u64) u64 {
-    const a = deltaswap3(x, shift, mask);
-    const b = deltaswap3(x, shift, mask);
-    if (a != b) unreachable;
-    return a;
-}
-export fn flipVertical(x: u64) u64 {
-    //var y = deltaswap(x, 8, 0x00FF00FF00FF00FF);
-    //y = deltaswap(y, 16, 0x0000FFFF0000FFFF);
-    //return deltaswap(y, 32, 0x00000000FFFFFFFF);
+fn vert(x: u64) u64 {
     return @byteSwap(u64, x);
 }
 
-export fn mirrorHorizontal(x: u64) u64 {
-    //var y = deltaswap(x, 1, 0x5555555555555555);
-    //y = deltaswap(y, 2, 0x3333333333333333);
-    //return deltaswap(y, 4, 0x0f0f0f0f0f0f0f0f);
+fn hori(x: u64) u64 {
     return @byteSwap(u64, @bitReverse(u64, x));
 }
 
-export fn flipDiagA8H1(x: u64) u64 {
-    var y = deltaswap(x, 9, 0xaa00aa00aa00aa00 >> 9);
-    y = deltaswap(y, 18, 0xcccc0000cccc0000 >> 18);
-    return deltaswap(y, 36, 0x0f0f_0f0f);
+fn diag(x: u64) u64 {
+    var y = deltaswap(x, 7, mul(0x55, 0xaa));
+    y = deltaswap(y, 14, mul(0x33, 0xcc));
+    return deltaswap(y, 28, mul(0x0f, 0xf0));
+}
+//antidiagonal
+fn gaid(x: u64) u64 {
+    var y = deltaswap(x, 9, mul(0x55, 0x55));
+    y = deltaswap(y, 18, mul(0x33, 0x33));
+    return deltaswap(y, 36, mul(0x0f, 0x0f));
 }
 
-export fn flipDiagA1H8(x: u64) u64 {
-    var y = deltaswap(x, 9, 0xaa00aa00aa00aa00 >> 9);
-    y = deltaswap(y, 18, 0xcccc0000cccc0000 >> 18);
-    return deltaswap(y, 36, 0x0f0f_0f0f);
-}
-
-fn testbit(t: comptime fn (u6) u6, u: comptime fn (u64) u64) bool {
+fn testbit(comptime t: fn (u6) u6, comptime u: fn (u64) u64) bool {
     comptime var i = 0;
     inline while (i < 64) : (i += 1)
         if (@as(u64, 1) << t(i) != u(@as(u64, 1) << i))
             return false;
     return true;
+}
+
+fn ver(a: u6) u6 {
+    return a ^ 56;
+}
+fn hor(a: u6) u6 {
+    return a ^ 7;
+}
+fn dia(a: u6) u6 {
+    return (a << 3) | (a >> 3);
+}
+fn aid(a: u6) u6 {
+    return (~a << 3) | (~a >> 3);
+}
+
+const expect = @import("std").testing.expect;
+test {
+    try expect(testbit(ver, vert));
+    try expect(testbit(hor, hori));
+    try expect(testbit(dia, diag));
+    try expect(testbit(aid, gaid));
 }
