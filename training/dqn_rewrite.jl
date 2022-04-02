f9eb4136ea74db0e8e17a538b155c05e8cc7b79f
leesongun <12179851+leesongun@users.noreply.github.com>
2022-04-03 07:48:45 +0900

    proof-of-concept dqn rewrite

---
+++ training/dqn_rewrite.jl
@@ -0,0 +1,97 @@
+include("./othello.jl");
+include("./model.jl");
+using Bits
+using Flux
+
+#toplane(a::UInt64) = reshape(bits(a), 8, 8, 1, 1)
+input(a::Game) = vcat(bits(a.a), bits(a.b))
+output(a::Game) = model(input(a))
+value(a::Game) = sum(output(a))
+
+#higher is better
+function against_random()
+    turn = rand(Bool)
+
+    g = init()
+    while notend(g)
+        if rawmoves(g) == 0
+            g = flip(g)
+        else
+            turn = !turn
+        end
+        if turn
+            move = model_agent(g, value)
+        else
+            move = rand_agent(g)
+        end
+        g = g + move
+    end
+    score = count_ones(g.a)
+    if turn
+        score = 64 - score
+    end
+    return score
+end
+
+
+model = Dense(128, 64, tanh)
+
+loss(x, y) = Flux.Losses.mse(model(x), y)
+opt = RADAMW(0.1, (0.9, 0.999), 1)
+epsilon = 0.9
+
+x_train = []
+y_train = []
+
+while true
+    global x_train, y_train
+
+    g = init()
+    while notend(g)
+        if rawmoves(g) == 0
+            g = flip(g)
+        end
+
+        pair = (x->(x,-output(g + x))).(moves(g))
+        maxmove = findmax((x->sum(x[2])).(pair))[2]
+        
+        if rand() < epsilon
+            move = rand(pair)[1]
+        else
+            move = pair[maxmove][1]
+        end
+        
+        push!(x_train, input(g))
+        push!(y_train, pair[maxmove][2])
+        
+        g = g + move
+    end
+    #push!(x_train, vcat(bits(a), bits(b)))
+    #push!(y_train, (x-> 2*x - 1).(bits(a)))
+
+    if length(x_train) > 256
+
+        t = (x->against_random()).(1:100)
+        print(sum(t)/length(t), "\n")
+
+        model |> gpu
+        global opt
+        
+        parameters = params(model)
+        x_train = hcat(x_train...)
+        y_train = hcat(y_train...)
+        data = [(x_train, y_train)]
+        evalcb() = @show(loss(x_train, y_train))
+
+        for epoch in 1:2
+            Flux.train!(loss, parameters, data, opt, cb=evalcb)
+        end
+        #open("model/weights.txt", "a+") do io
+        #    write(io, string(params(model)))
+        #end;
+
+        model |> cpu
+        x_train = []
+        y_train = []
+    end
+end
--- training/othello.jl
+++ training/othello.jl
@@ -1,3 +1,6 @@
+using Bits
+include("./ffi.jl");
+include("./bitboard.jl");
 struct Game
     a::UInt64
     b::UInt64
@@ -12,6 +15,7 @@ function Base.:+(a::Game, b::UInt8)
 end
 
 moves(a::Game) = bitboard_to_array(rawmoves(a))
+num_moves(a::Game) = count_ones(rawmoves(a))
 rawmoves(a::Game) = Othello.moves(a.a, a.b)
 notend(a::Game) = rawmoves(a) != 0 || rawmoves(flip(a)) != 0
 flip(a::Game) = Game(a.b, a.a)
@@ -35,4 +39,20 @@ function playwhile(agent1, agent2, logger)
         logger(g, move, turn)
         g = g + move
     end
+    return g
+end
+
+rand_agent(a::Game) = rand(moves(a))
+good_agent(a::Game) = model_agent(a, num_moves)
+
+function model_agent(a::Game, value)
+    list = moves(a)
+    maxmove = findmin(x -> value(a + x), list)[2]
+    return list[maxmove]
+end
+
+function value_move(a::Game, value)
+    list = moves(a)
+    mm = findmin(x -> value(a + x), list)
+    return (-mm[1], list[mm[2]])
 end
