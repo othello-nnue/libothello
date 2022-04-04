5dc392c308c1641dd7f72b2c8b9f4c0c8eb935af
leesongun <12179851+leesongun@users.noreply.github.com>
2022-04-04 10:40:21 +0900

    faster ffi

--- game/ffi.zig
+++ game/ffi.zig
@@ -1,6 +1,7 @@
 const game = @import("othello");
-
+//https://github.com/ziglang/zig/issues/11312
 export fn flip(a: u64, b: u64, c: u8) u64 {
+    if (@truncate(u6, c) != c) unreachable;
     const x = game{ .board = .{ a, b } };
     return x.flip(@intCast(u6, c));
 }
