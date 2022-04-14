fba13953b9e68a7f46aa461f60577ac5e68a89e1
leesongun <12179851+leesongun@users.noreply.github.com>
2022-04-14 13:10:58 +0900

    find out why slow

--- arm64/mask.zig
+++ arm64/mask.zig
@@ -6,8 +6,8 @@ fn mask(pos: u6, comptime dir: u6) u64 {
     return fill(t, dir) & -%t << 1;
 }
 
-fn _mask(pos: u6) @Vector(4, u64) {
+fn _mask(pos: u6) [4]u64 {
     return .{ mask(pos, 1), mask(pos, 8), mask(pos, 9), mask(pos, 7) };
 }
 
-pub const MASK = @import("utils").make_array(@Vector(4, u64), _mask);
+pub const MASK = @import("utils").make_array([4]u64, _mask);
