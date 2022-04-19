aae5427cefdd4279c86c51f3657945c90e533ba8
leesongun <12179851+leesongun@users.noreply.github.com>
2022-04-19 17:10:00 +0900

    add llvm intrinsics

--- amd64/intrinsic.zig
+++ amd64/intrinsic.zig
@@ -4,6 +4,15 @@ extern fn @"llvm.x86.bmi.pdep.64"(u64, u64) u64;
 extern fn @"llvm.x86.bmi.pext.64"(u64, u64) u64;
 extern fn @"llvm.assume"(bool) void;
 
+//todo : ARM SVE2
+const vscale = undefined;
+extern fn @"llvm.aarch64.sve.bdep.x.nx2i64"(@Vector(2 * vscale, u64), @Vector(2 * vscale, u64)) @Vector(2 * vscale, u64);
+extern fn @"llvm.aarch64.sve.bext.x.nx2i64"(@Vector(2 * vscale, u64), @Vector(2 * vscale, u64)) @Vector(2 * vscale, u64);
+
+//todo : RISC-V
+extern fn @"llvm.riscv.bdepw.i64"(u64, u64) u64;
+extern fn @"llvm.riscv.bextw.i64"(u64, u64) u64;
+
 // export LLVM pdep/pext intrinsic
 pub inline fn pext(a: u64, b: u64) u64 {
     return @"llvm.x86.bmi.pext.64"(a, b);
